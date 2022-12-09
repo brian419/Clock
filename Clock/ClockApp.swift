b3fb46da4fd9985fcef3519462d3d7c919492caa
brian419 <json10@crimson.ua.edu>
2022-12-09 07:02:04 -0600

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Brian Son on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
